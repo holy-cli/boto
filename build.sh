abb7a80efc8241d5deb660139976e73743ab1340
fire015 <emailfire@gmail.com>
2023-09-14 16:57:39 +0100

    Create virtual environment

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ export BOTOCORE_VERSION=1.31.47
 export BOTO3_VERSION=1.28.47
 
 # Create virtual environment
-# python3 -m venv .env
-# source .env/bin/activate
-# pip install --upgrade setuptools build wheel
+python3 -m venv .env
+source .env/bin/activate
+pip install --upgrade setuptools build wheel
 
 # Clone botocore source code
 git clone --depth 1 --branch $BOTOCORE_VERSION https://github.com/boto/botocore.git
